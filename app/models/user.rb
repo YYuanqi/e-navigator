539961040b50c8aee962c6c2492ade8a967f8c30
EC2 Default User <ec2-user@ip-172-31-24-140.us-east-2.compute.internal>
2018-10-23 14:17:24 +0000

    ユーザーの登録/ログイン機能の実装の指摘反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: {with: VALID_EMAIL_REGEX},
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil:true
-  enum gender: {"":0, Male: 1, Female: 2 }
+  enum gender: { other: 0, male: 1, female: 2 }
   validates :school, length: { maximum: 50 }
 end
