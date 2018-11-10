b527c87077145480c38c56eeec67cf29059ab59a
YYuanqi <gzcyyq@gmail.com>
2018-11-10 08:39:58 +0000

    replace @user with current_user in views

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,7 +1,8 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  
   validates :user_id, presence: true
   validates :time, presence: true
   validates :status, presence: true
-  enum gender: { 保留: 0, 承認: 1, 却下: 2 }
+
 end
