f8663cf190b980e0b121718d035f3513be006bda
YYuanqi <gzcyyq@gmail.com>
2018-11-10 09:45:02 +0000

    find interview by id directly

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -10,7 +10,7 @@ class InterviewsController < ApplicationController
   end
   
   def create
-    @interview = current_user.interviews.new(interview_params)
+    @interview = Interview.find(params[:id])
     if @interview.save
       flash[:success] = "面接が作成されました"
       redirect_to user_interviews_url
@@ -20,11 +20,11 @@ class InterviewsController < ApplicationController
   end
   
   def edit
-    @interview = current_user.interviews.find(params[:id])
+    @interview = Interview.find(params[:id])
   end
   
   def update
-    @interview = current_user.interviews.find(params[:id])
+    @interview = Interview.find(params[:id])
     if @interview.update_attributes(interview_params)
       flash[:success] = "面接が更新されました"
       redirect_to user_interviews_url
@@ -34,7 +34,7 @@ class InterviewsController < ApplicationController
   end
   
   def destroy
-    @interview = current_user.interviews.find(params[:id])
+    @interview = Interview.find(params[:id])
     @interview.destroy
     flash[:success] = "面接が削除されました"
     redirect_to user_interviews_url
